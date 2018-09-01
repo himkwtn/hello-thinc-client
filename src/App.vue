c6a9d5eef8907201d8a951321ce1fb00a0c83005
him k <him10424@gmail.com>
2018-09-01 10:40:56 +0700

    asd

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,65 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <mdc-layout-app>
+    <mdc-top-app-bar v-bind:title="this.$route.name" event="nav" slot="toolbar">
+      <mdc-top-app-bar-action icon="power_settings_new" v-if="loggedIn" @click="logout"></mdc-top-app-bar-action>
+    </mdc-top-app-bar>
+    <mdc-drawer slot="drawer" toggle-on="nav">
+        <mdc-drawer-list>
+          <mdc-drawer-item to="/" start-icon="home">Home</mdc-drawer-item>
+          <mdc-drawer-item to="/login" start-icon="supervised_user_circle">Admin</mdc-drawer-item>
+        </mdc-drawer-list>
+    </mdc-drawer>
+    <main class="main">
+      <router-view :loggedIn="loggedIn"/>
+    </main>
+  </mdc-layout-app>
 </template>
+
+<script>
+import 'vue-mdc-adapter/dist/vue-mdc-adapter.css';
+import { EventBus } from './event-bus'
+import { ProductService } from '@/common/api.service';
+export default {
+  name: 'App',
+  data () {
+    return {
+      loggedIn:false
+    }
+  },
+  methods: {
+		logout: async function() {
+			const result = await ProductService.logout()
+			this.loggedIn = false
+			this.$router.push('/login');
+
+		}
+  },
+  created() {
+    EventBus.$on('login', () => {
+      this.loggedIn = true
+		})
+  }
+};
+</script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  background-color: #ededed;
   color: #2c3e50;
 }
-#nav {
-  padding: 30px;
+.container {
+  width: 80%;
+  margin: 0 auto;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.mdc-layout-app--main-container {
+  margin-top: 64px;
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.main {
+  max-width: 80%;
+  margin: 0 auto;
+  transition: width 0.3s;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,26 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import VueSocketIO from 'vue-socket.io';
+import ApiService from '@/common/api.service';
+import ErrorFilter from '@/common/error.filter';
+import VueMDCAdapter from 'vue-mdc-adapter';
+ApiService.init();
+const url =
+  process.env.NODE_ENV == 'development'
+    ? 'http://localhost:3000'
+    : window.location.host;
+Vue.use(VueMDCAdapter);
+Vue.use(VueSocketIO, url);
+Vue.filter('error', ErrorFilter);
 
 Vue.config.productionTip = false;
-
+console.log;
 new Vue({
+  el: '#app',
   router,
-  render: h => h(App)
-}).$mount("#app");
+  components: { App },
+  template: '<App/>'
+});
